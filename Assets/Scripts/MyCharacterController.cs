56014cf504dce98fd209824a7d53d512c4b382ab
Paul Craven <paul@cravenfamily.com>
2022-03-02 10:05:44 -0600

    Work on setup for next class.

--- Assets/Scripts/MyCharacterController.cs
+++ Assets/Scripts/MyCharacterController.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MyCharacterController : MonoBehaviour
 {
+    public int score = 0;
+
     Rigidbody2D body;
+
 
     float horizontal;
     float vertical;
@@ -14,18 +18,22 @@ public class MyCharacterController : MonoBehaviour
 
     void Start()
     {
+        // Get the rigid body component for the player character.
+        // (required to have one)
         body = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        // Gives a value between -1 and 1
-        horizontal = Input.GetAxisRaw("Horizontal"); // -1 is left
-        vertical = Input.GetAxisRaw("Vertical"); // -1 is down
+        // Get our axis values
+        horizontal = Input.GetAxisRaw("Horizontal"); 
+        vertical = Input.GetAxisRaw("Vertical"); 
     }
 
     void FixedUpdate()
     {
+
+        // If player is running diagonally, we don't want them to move extra-fast.
         if (horizontal != 0 && vertical != 0) // Check for diagonal movement
         {
             // limit movement speed diagonally, so you move at 70% speed
@@ -33,10 +41,32 @@ public class MyCharacterController : MonoBehaviour
             vertical *= moveLimiter;
         }
 
+        // Set player velocity
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
     }
-    void OnTriggerEnter(Collider other)
+
+    void OnTriggerEnter2D(Collider2D colliderEvent)
     {
-        Debug.Log("Trigger");
+        // Did we run into an object that will affect our score?
+        ScoreScript scoreObject = colliderEvent.gameObject.GetComponent(typeof(ScoreScript))
+                                  as ScoreScript;
+
+        if (scoreObject != null)
+        {
+            // Yes, change the score
+            score += scoreObject.points;
+            // Destroy the object
+            Destroy(colliderEvent.gameObject);
+        }
+
+        // Did we run into an object that will cause a scene change?
+        SceneChangeScript sceneChangeObject = colliderEvent.gameObject.GetComponent(typeof(SceneChangeScript))
+                                              as SceneChangeScript;
+        if (sceneChangeObject != null) {
+            // Yes, get our current scene index
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            // Load up the scene accourding to the sceneChange value
+            UnityEngine.SceneManagement.SceneManager.LoadScene(currentSceneIndex + sceneChangeObject.sceneChange);
+        }
     }
 }
---
+++ Assets/Scripts/SceneChangeScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChangeScript : MonoBehaviour
+{
+    // Default so we go to the next scene. Use -1 to go back a scene.
+    public int sceneChange = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
