673f73f701eddb21e0ce29df2004fcbd710e12c6
Peter Walkwitz <peter.walkwitz@my.simpson.edu>
2022-03-03 13:39:16 -0600

    One Enemy Moving, added script for it - Peter

---
+++ Assets/Scripts/Peter_Enemy_Script.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Peter_Enemy_Script : MonoBehaviour
+{
+    // Game object that will be moving
+    //public GameObject game_object;
+
+    // Generic varibales to move the enemy between two points (including diagonally)
+    public int move_speed = 2;
+    public float left_x = 0;
+    public float right_x = 0;
+    public float top_y = 0;
+    public float bottom_y = 0;
+
+    int move_horizontal = 0;
+    int move_vertical = 0;
+
+    void FixedUpdate()
+    {
+
+        //float moveHorizontal = direction;
+
+        Vector3 movement = new Vector3(move_horizontal, move_vertical);
+        GetComponent<Rigidbody2D>().velocity = movement * move_speed;
+
+        // Checking x values (to not move x axis make each = "spawn" position)
+        if (transform.position.x < left_x)
+        {
+            move_horizontal = 1;
+        }
+        if (transform.position.x > right_x)
+        {
+            move_horizontal = -1;
+        }
+        
+        // Checking y values (to not move y axis make each = "spawn" position)
+        if (transform.position.x > top_y)
+        {
+            move_vertical = -1;
+        }
+        if (transform.position.x < bottom_y)
+        {
+            move_vertical = 1;
+        }
+    }
+
+}
