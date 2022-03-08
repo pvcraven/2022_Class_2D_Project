7b04421e8c4289b6b7d63e2c171f42d6f18628c4
Sammy Cardinal <sammy.cardinal@my.simpson.edu>
2022-03-08 11:34:17 -0600

    added script and suh

---
+++ Assets/Scripts/SammysScripts/NonPlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// public class NonPLayerMovement : MonoBehaviour
+// {
+    // Variables
+    // public int speed = 1;
+    // public float left = 3;
+    // public float right = 3;
+
+    // voidFixedUpdate()
+    // {
+        // Vector3 movement = new Vector3(left, right = movement * speed);
+
+        // if(transform.position.x < left)
+        // {
+           // movement = 1;
+        //}
+       // if(transform.position.x > right)
+       // {
+       //     movement = -1;
+       // }
+        
+    //}
+//}
