a327fcdcb48728da34a2fcd59808d9db7a44fd25
Zambrose25 <zrambro1@gmail.com>
2022-03-08 14:40:22 -0600

    Create tile palette, implement character script, work on enemy movement and respawn

---
+++ Assets/Scripts/ZachAmbrose_Scripts/ZachAmbrose_Character_Script.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ZachAmbrose_Character_Script : MonoBehaviour
+{
+    public int score = 0;
+
+    Rigidbody2D body;
+
+    //Vector3 radishPosition;
+    //public GameObject() radish;
+
+    float horizontal;
+    float vertical;
+    float moveLimiter = 0.7f;
+
+    public Vector3 spawnPosition;
+    public float runSpeed = 5.0f;
+
+    void Start()
+    {
+        // Get the rigid body component for the player character.
+        // (required to have one)
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Get our axis values
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+
+        // If player is running diagonally, we don't want them to move extra-fast.
+        if (horizontal != 0 && vertical != 0) // Check for diagonal movement
+        {
+            // limit movement speed diagonally, so you move at 70% speed
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+
+        // Set player velocity
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+
+    void OnTriggerEnter2D(Collider2D colliderEvent)
+    {
+        // Did we run into an object that will affect our score?
+        ScoreScript scoreObject = colliderEvent.gameObject.GetComponent(typeof(ScoreScript))
+                                  as ScoreScript;
+
+        if (scoreObject != null)
+        {
+            // Yes, change the score
+            score += scoreObject.points;
+            // Destroy the object
+            //radishPosition = gameObject.transform.position;
+            Destroy(colliderEvent.gameObject);
+        }
+
+        // Did we run into an object that will cause a scene change?
+        SceneChangeScript sceneChangeObject = colliderEvent.gameObject.GetComponent(typeof(SceneChangeScript))
+                                              as SceneChangeScript;
+        if (sceneChangeObject != null)
+        {
+            // Yes, get our current scene index
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            // Load up the scene accourding to the sceneChange value
+            UnityEngine.SceneManagement.SceneManager.LoadScene(currentSceneIndex + sceneChangeObject.sceneChange);
+        }
+    }
+    void OnGUI()
+    {
+        // Dispaly our score
+        GUIStyle guiStyle = new GUIStyle(GUI.skin.label);
+        guiStyle.fontSize = 18; //modify the font height
+        GUI.Label(new Rect(10, 10, 100, 50), "Score: " + score, guiStyle);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Enemy") && score < 14)
+        {
+            gameObject.transform.position = spawnPosition;
+            score = 0;
+          //  Instantiate(radish, radishPosition);
+        } 
+        if (col.gameObject.tag.Equals("Enemy") && score >= 14) {
+            col.gameObject.SetActive(false);
+            Debug.Log("hello");
+        }
+    }
+}
