561cef200ea70978ece597c9e7ea1e422e0b20e5
Isaac Roe <isaac.roe@my.simpson.edu>
2022-04-07 11:07:22 -0500

    Added bullet

---
+++ Assets/Scripts/Isaac_Scripts/Bullet_Isaac.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Isaac : MonoBehaviour
+{
+    Vector3 _origin;
+    public float maxDistance = 8.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _origin = transform.position;
+        Debug.Log("Is Working");
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger");
+        if (collision.tag == "Destroyable")
+        {
+            Debug.Log("Destroyable");
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector2.Distance(_origin, transform.position);
+
+        if (distance > maxDistance)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
