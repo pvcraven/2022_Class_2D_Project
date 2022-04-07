38787227b45cc11acf4009852225756229e78a71
Peter Walkwitz <peter.walkwitz@my.simpson.edu>
2022-04-07 14:09:15 -0500

    Fixed Naming Sammy Bullet Script Error

--- Assets/Scripts/SammysScripts/SammyBullet.cs
+++ Assets/Scripts/SammysScripts/SammyBullet.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletScript : MonoBehaviour
+public class SammyBulletScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
