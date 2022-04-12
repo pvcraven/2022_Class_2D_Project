c2256594ce3acbc33caf416a26d84955bde6a15e
Zach Wemhoff <zach.wemhoff@my.simpson.edu>
2022-04-12 11:28:59 -0500

    2D Attacks Class Work

---
+++ Assets/Scripts/ZachWemhoff_Scripts/WemhoffEnemyScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WemhoffEnemyScript : MonoBehaviour
+{
+    public int health;
+    public int speed;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        //Instantiate(transform.position, Quaternion.identity);
+        health -= damage;
+        Debug.Log("damage TAKEN!");
+    }
+}
