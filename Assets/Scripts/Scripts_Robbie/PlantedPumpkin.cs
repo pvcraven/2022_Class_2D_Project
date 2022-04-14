9ca29d91b8c1c99cc14afc6ac2f90177d09a904b
Robbie Wagner <robbie.wagner@my.simpson.edu>
2022-04-14 10:16:30 -0500

    Ingrained pumpkins give you points

--- Assets/Scripts/Scripts_Robbie/PlantedPumpkin.cs
+++ Assets/Scripts/Scripts_Robbie/PlantedPumpkin.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class PlantedPumpkin : MonoBehaviour
 {
 
+    public Character_Robbie character;
     public float health;
+
+    public GameObject pumpkinPrefab;
+    public Rigidbody2D body;
+
+    public int scoreHandOff;
 
     // Update is called once per frame
     void Update()
     {
         if(health <= 0)
         {
+            character.score += scoreHandOff;
             Destroy(gameObject);
         }
+    }
+
+    public IEnumerator ShakePumpkin()
+    {
+        yield return new WaitForSeconds(1f);
+        StopCoroutine(ShakePumpkin());
     }
 }
